599024be3841e4da18f6fbe3593d58f4c95c4d59
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-03-30 16:44:39 +0600

    local storage and react tost add complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
 import SideCart from "./components/SideCart/SideCart";
 import Home from "./components/Home/Home";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [watchTime, setWatchTime] = useState("");
 
   const handleWatchTime = (time) => {
-    const previousWatchTime = localStorage.getItem('watchTime');
-    // console.log(previousWatchTime);
+    const previousWatchTime = JSON.parse(localStorage.getItem("watchTime"));
+    // second time set the watch time in local storage
     if (previousWatchTime) {
-      
-    }
-    else {
-      localStorage.setItem('watchTime', time);
+      const totalTime = previousWatchTime + time;
+      //set the watch time in local storage
+      localStorage.setItem("watchTime", totalTime);
+      setWatchTime(totalTime);
+    } else {
+      // first time set the watch time in local storage
+      localStorage.setItem("watchTime", time);
+      setWatchTime(time);
     }
   };
   return (
@@ -30,9 +37,10 @@ function App() {
           <Home handleWatchTime={handleWatchTime}></Home>
         </div>
         <div className="sideCart col-md-4 card">
-          <SideCart></SideCart>
+          <SideCart watchTime={watchTime}></SideCart>
         </div>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const SideCart = () => {
+const SideCart = ({ watchTime }) => {
+  const [time, setTime] = useState(watchTime);
+  const [breakTime, setBreakTime] = useState(0);
+
+  useEffect(() => {
+    // get the Total watch time from local storage
+    const getWatchTime = localStorage.getItem("watchTime");
+    // console.log(getWatchTime);
+    // pass the getWatchTime variable with stored values in setTimeout
+    setTime(getWatchTime);
+    const getBreakTime = localStorage.getItem("breakTime");
+    // if (getBreakTime === null) {
+    //   setBreakTime(0);
+    // } else {
+    // }
+    setBreakTime(getBreakTime);
+  }, [watchTime]);
+
+  const handleBreakTime = (bTime) => {
+    // console.log("object", bTime);
+    localStorage.setItem("breakTime", bTime);
+    setBreakTime(bTime);
+  };
+  const handleComplete = () => {
+    toast("Wow so easy!");
+  };
   return (
     <div>
       <h1>My Cart</h1>
 
       <div className="mt-5 text-center">
         <p>Total Watch Time </p>
-        <input type="text" value={0} disabled />
+        <input type="text" value={time} disabled />
       </div>
       <h5 className="mt-5">Add Break time</h5>
-      <button className="w-25 btn-circle m-1 btn btn-info">15</button>
-      <button className="w-25 btn-circle bg-warning btn btn-info">20</button>
-      <button className="w-25 btn-circle m-1 bg-danger btn btn-info">25</button>
-      <input type="text" value={0} disabled />
-      <button className="mt-5 btn btn-info w-100">Complete</button>
+      <button
+        onClick={() => handleBreakTime(15)}
+        className="w-25 btn-circle m-1 btn btn-info"
+      >
+        15
+      </button>
+      <button
+        onClick={() => handleBreakTime(20)}
+        className="w-25 btn-circle bg-warning btn btn-info"
+      >
+        20
+      </button>
+      <button
+        onClick={() => handleBreakTime(25)}
+        className="w-25 btn-circle m-1 bg-danger btn btn-info"
+      >
+        25
+      </button>
+      <input type="text" value={breakTime} disabled />
+      <button onClick={handleComplete} className="mt-5 btn btn-info w-100">
+        Complete
+      </button>
     </div>
   );
 };
